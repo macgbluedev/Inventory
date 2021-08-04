e181390e11caf0134f533e5d745b43a93f4c7684
macg30 <macg30@users.noreply.github.com>
2021-08-04 18:41:52 +0200

    Add CreateProductComponent in Index, functional form for Product

--- Inventory/Startup.cs
+++ Inventory/Startup.cs
@@ -31,6 +31,7 @@ namespace Inventory
 			services.AddServerSideBlazor();
 			services.AddSingleton<WeatherForecastService>();
 			services.AddSingleton<B_Product>();
+			services.AddSingleton<B_Category>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
